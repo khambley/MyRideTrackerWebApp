cf237e1dcde199ed14d0e8832fd0c6de5456edf8
Katherine Hambley <superdesigngirl@mac.com>
2020-05-16 16:53:29 -0500

    Added Ride model and DbContext

---
+++ MyRideTrackerWebApp/MyRideTrackerWebApp/Data/RideDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyRideTrackerWebApp.Models;
+
+namespace MyRideTrackerWebApp.Data
+{
+	public class RideDbContext : DbContext
+	{
+		public RideDbContext (DbContextOptions<RideDbContext> options) : base(options) { }
+
+		public DbSet<Ride> Rides { get; set; }
+	}
+}
