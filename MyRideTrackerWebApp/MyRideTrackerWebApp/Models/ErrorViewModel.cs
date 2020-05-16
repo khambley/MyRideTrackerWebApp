b845ab33223f0ecfbf4c88d9ccdfe24457c0bf45
Katherine Hambley <superdesigngirl@mac.com>
2020-05-16 11:26:01 -0500

    Added initial project files

---
+++ MyRideTrackerWebApp/MyRideTrackerWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyRideTrackerWebApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ MyRideTrackerWebApp/MyRideTrackerWebApp/Models/Ride.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRideTrackerWebApp.Models
+{
+	public class Ride
+	{
+		public int RideId { get; set; }
+		[DataType(DataType.Date)]
+		public DateTime RideDate { get; set; }
+		public decimal RideDistance { get; set; }
+		public string RideRoute { get; set; }
+		public string RideDescription { get; set; }
+
+	}
+}
