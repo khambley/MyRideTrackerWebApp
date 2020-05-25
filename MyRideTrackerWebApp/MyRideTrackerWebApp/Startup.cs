f9ab846fa748db11aab58b6f6744ec973da09808
Katherine Hambley <superdesigngirl@mac.com>
2020-05-24 19:35:06 -0500

    added dev errors to prod

--- MyRideTrackerWebApp/MyRideTrackerWebApp/Startup.cs
+++ MyRideTrackerWebApp/MyRideTrackerWebApp/Startup.cs
@@ -33,7 +33,7 @@ namespace MyRideTrackerWebApp
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
+			if (env.IsProduction())
 			{
 				app.UseDeveloperExceptionPage();
 			}
